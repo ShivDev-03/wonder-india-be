7cd692617b9ebd0e5f367cdc3fe9dcfea41d1c08
Shravan <shravan@matlabinfotech.com>
2026-04-26 17:38:00 +0530

    fix build issue

--- routes/common/docs/swagger.route.js
+++ routes/common/docs/swagger.route.js
@@ -1,8 +1,11 @@
+import path from 'path';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
-const swaggerDocument = YAML.load('docs/opencollection.yml');
+// Resolve from this file so it works from both repo root and `build/` (cwd-independent)
+const specPath = path.join(__dirname, '../../../docs/opencollection.yml');
+const swaggerDocument = YAML.load(specPath);
 const router = express.Router();
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 module.exports = router;
